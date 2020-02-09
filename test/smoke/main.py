a565cd614d98e0a2fd0e0591f429c6c4c1d12d40
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-02-09 18:44:05 -0500

    remove test code

--- test/smoke/main.py
+++ test/smoke/main.py
@@ -1,12 +1,12 @@
 #!/bin/python
 
 from chains import Binance
+
 from transaction import Transaction
 from coin import Coin
 
 def main():
-    txn = Transaction(Binance.chain, "bnb1", "bnb2", Coin("BNB", 25), "my memo")
-    print(txn)
+    pass
 
 if __name__ == "__main__":
     main()
