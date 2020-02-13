2fdfb451775cc6c87aac9c139165ad4a786d7c16
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-02-13 15:11:37 -0500

    debuging

--- test/smoke/common.py
+++ test/smoke/common.py
@@ -3,7 +3,7 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 def requests_retry_session(
-    retries=3,
+    retries=10,
     backoff_factor=1,
     status_forcelist=(500, 502, 504),
     session=None,
