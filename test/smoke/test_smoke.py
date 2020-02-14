add5095924c4c0e88986507c369b5e8c7c220919
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-02-14 16:43:21 -0500

    support updating balances.json

--- test/smoke/test_smoke.py
+++ test/smoke/test_smoke.py
@@ -78,6 +78,8 @@ class TestSmoke(unittest.TestCase):
         if failure:
             raise Exception("Fail")
 
+        if failure:
+            raise Exception("Fail")
 
 if __name__ == "__main__":
     unittest.main()
