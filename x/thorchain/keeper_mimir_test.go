eb292b6583a051a864559da72c177ed6e61e14c2
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-05-17 01:50:45 -0400

    [feature] Release the KRAKEN

--- x/thorchain/keeper_mimir_test.go
+++ x/thorchain/keeper_mimir_test.go
@@ -20,4 +20,17 @@ func (s *KeeperMimirSuite) TestMimir(c *C) {
 	val, err = k.GetMimir(ctx, "bogus")
 	c.Assert(err, IsNil)
 	c.Check(val, Equals, int64(-1))
+
+	// test that releasing the kraken removes previously set key/values
+	k.SetMimir(ctx, KRAKEN, 0)
+	val, err = k.GetMimir(ctx, "foo")
+	c.Assert(err, IsNil)
+	c.Assert(val, Equals, int64(-1))
+
+	// test that we cannot put the kraken back in the cage
+	k.SetMimir(ctx, KRAKEN, -1)
+	k.SetMimir(ctx, "foo", 33)
+	val, err = k.GetMimir(ctx, "foo")
+	c.Assert(err, IsNil)
+	c.Assert(val, Equals, int64(-1))
 }
