621be98ce1518806764855e68d1927c563d6749b
Heimdall Thor <heimdallthor02@gmail.com>
2020-06-05 20:28:58 +1000

    use amino to unmarshal binance broadcast response

--- bifrost/thorclient/types/commit.go
+++ bifrost/thorclient/types/commit.go
@@ -1,35 +1,10 @@
 package types
 
 import (
-	abci "github.com/tendermint/tendermint/abci/types"
-	"github.com/tendermint/tendermint/libs/bytes"
-	"gitlab.com/thorchain/thornode/common"
+	coretypes "github.com/tendermint/tendermint/rpc/core/types"
 )
 
-type BadCommit struct {
-	Height string      `json:"height"`
-	TxHash common.TxID `json:"txhash"`
-	Code   int         `json:"code"`
-	Log    string      `json:"raw_log"`
-}
-
-type Commit struct {
-	Height string      `json:"height"`
-	TxHash common.TxID `json:"txhash"`
-	Logs   []struct {
-		Success bool   `json:"success"`
-		Log     string `json:"log"`
-	} `json:"logs"`
-}
-
-type ResultBroadcastTxCommit struct {
-	CheckTx   abci.ResponseCheckTx   `json:"check_tx"`
-	DeliverTx abci.ResponseDeliverTx `json:"deliver_tx"`
-	Hash      bytes.HexBytes         `json:"hash"`
-	Height    string                 `json:"height"`
-}
-
 type BroadcastResult struct {
-	JSONRPC string                  `json:"jsonrpc"`
-	Result  ResultBroadcastTxCommit `json:"result"`
+	JSONRPC string                            `json:"jsonrpc"`
+	Result  coretypes.ResultBroadcastTxCommit `json:"result"`
 }
