8096673ac73f02e2ce5a78a41e633e534cb2fe05
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-06-17 22:00:46 -0400

    remove debug lines

--- x/thorchain/keeper/v1/keeper_ragnarok.go
+++ x/thorchain/keeper/v1/keeper_ragnarok.go
@@ -1,8 +1,6 @@
 package keeperv1
 
 import (
-	"fmt"
-
 	cosmos "gitlab.com/thorchain/thornode/common/cosmos"
 )
 
@@ -40,8 +38,6 @@ func (k KVStore) GetRagnarokNth(ctx cosmos.Context) (int64, error) {
 	key := k.GetKey(ctx, prefixRagnarokNth, "")
 	store := ctx.KVStore(k.storeKey)
 	if !store.Has([]byte(key)) {
-		fmt.Println("no rag nth found")
-		fmt.Printf("Get Nth: %s: %d\n", key, 0)
 		return 0, nil
 	}
 	var ragnarok int64
@@ -50,14 +46,12 @@ func (k KVStore) GetRagnarokNth(ctx cosmos.Context) (int64, error) {
 	if err != nil {
 		return 0, dbError(ctx, "Unmarshal: ragnarok nth", err)
 	}
-	fmt.Printf("Get Nth: %s: %d\n", key, ragnarok)
 	return ragnarok, nil
 }
 
 func (k KVStore) SetRagnarokNth(ctx cosmos.Context, nth int64) {
 	store := ctx.KVStore(k.storeKey)
 	key := k.GetKey(ctx, prefixRagnarokNth, "")
-	fmt.Printf("Set Nth: %s: %d\n", key, nth)
 	store.Set([]byte(key), k.cdc.MustMarshalBinaryBare(nth))
 }
 
