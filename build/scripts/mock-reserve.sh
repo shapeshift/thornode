6f4e41e4ae6664e139b95a5824e7344ced557e68
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-08-12 16:38:55 +0200

    [tooling] add scripts to mock add/reserve to mock binance

---
+++ build/scripts/mock-reserve.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# ./mock-reserve.bash <mock binance IP address> <from_address> <num of rune>
+# ./mock-reserve.bash 127.0.0.1 bnbZXXXX 22000000000000000
+
+set -ex
+
+if [ -z $1 ]; then
+    echo "Missing mock binance address (address:port)"
+    exit 1
+fi
+
+if [ -z $2 ]; then
+    echo "Missing bnb address argument"
+    exit 1
+fi
+
+if [ -z $3 ]; then
+    echo "Missing rune amount"
+    exit 1
+fi
+
+POOL_ADDRESS=$(curl -s $1:1317/thorchain/pool_addresses | jq -r ".current[0].address")
+echo $POOL_ADDRESS
+
+curl -s -X POST -d "[{
+  \"from\": \"$2\",
+  \"to\": \"$POOL_ADDRESS\",
+  \"coins\":[
+      {\"denom\": \"RUNE-A1F\", \"amount\": $3}
+  ],
+  \"memo\": \"RESERVE\"
+}]" $1:26660/broadcast/easy
