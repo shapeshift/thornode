f7a0b80dbc6059b07b1964c8e6003cab514c67f6
Heimdall <5053194-heimdallthor@users.noreply.gitlab.com>
2021-02-16 04:31:34 +0000

    1. Update genesis script to only mint 1m RUNE to reserve on testnet 2. Don't return bond reward for node doesn't have bond

--- x/thorchain/mimir_address_testnet.go
+++ x/thorchain/mimir_address_testnet.go
@@ -5,5 +5,7 @@ package thorchain
 // ADMINS hard coded admin address
 var ADMINS = []string{
 	"tthor1x0akdepu6vs40cv30xqz3qnd85mh7gkfs27j7q",
+	// TODO remove the above address when we deploy next testnet , it is old
 	"tthor1xghvhe4p50aqh5zq2t2vls938as0dkr2l4e33j",
+	"tthor19pkncem64gajdwrd5kasspyj0t75hhkpy9zyej",
 }
