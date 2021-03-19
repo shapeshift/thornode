39c5d5db18f16593beb5c98ac96b058b26e7bac9
Heimdall <5053194-heimdallthor@users.noreply.gitlab.com>
2021-03-19 11:21:59 +0000

    tweak hard fork script

--- build/scripts/hard-fork.sh
+++ build/scripts/hard-fork.sh
@@ -8,20 +8,10 @@ cp -r ~/.thornode/config ~/.thornode/config.$DATE.bak
 cp -r ~/.thornode/data ~/.thornode/data.$DATE.bak
 
 # export genesis file
-thornode export --height $HARDFORK_BLOCK_HEIGHT > thorchain_genesis_export.$DATE.json
+thornode export --height $HARDFORK_BLOCK_HEIGHT >thorchain_genesis_export.$DATE.json
 
-# delete existing data files
-rm -rf ~/.thornode/data/application.db ~/.thornode/data/blockstore.db ~/.thornode/data/cs.wal ~/.thornode/data/evidence.db ~/.thornode/data/snapshots ~/.thornode/data/state.db ~/.thornode/data/tx_index.db ~/.thornode/data/priv_validator_state.json
-
-# delete old genesis file
-rm -rf ~/.thornode/config/genesis.json
-
-# if we need to run some command to change the state
-echo '{
-  "height": "0",
-  "round": 0,
-  "step": 0
-}' > ~/.thornode/data/priv_validator_state.json
+# reset the database
+thornode unsafe-reset-all
 
 # copied exported genesis file to the config directory
 cp thorchain_genesis_export.$DATE.json ~/.thornode/config/genesis.json
