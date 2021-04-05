3bdf89d0f004d2f3a6f01333a30f3d80628f5eb5
Heimdall <5053194-heimdallthor@users.noreply.gitlab.com>
2021-04-05 23:39:24 +0000

    Resolve "update testnet state , prepare testnet re-launch image"

--- constants/constants_testnet.go
+++ constants/constants_testnet.go
@@ -7,12 +7,12 @@ func init() {
 	int64Overrides = map[ConstantName]int64{
 		PoolCycle:             1000,
 		MinRunePoolDepth:      100_00000000,
-		AsgardSize:            6,
+		AsgardSize:            15,
 		DesiredValidatorSet:   30,
 		ChurnInterval:         240,
 		BadValidatorRate:      2048,
 		OldValidatorRate:      2048,
-		MinimumBondInRune:     10000_00000000, // 1 rune
+		MinimumBondInRune:     10000_00000000, // 10K rune
 		LiquidityLockUpBlocks: 0,
 		StagedPoolCost:        10_00000000,
 	}
