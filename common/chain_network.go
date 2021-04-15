528dfa26179db07225a1bbb0742becafd3a62405
Son of Odin <2700975-son-of-odin@users.noreply.gitlab.com>
2021-04-15 22:06:42 +0000

    [bugfix] detect chain network for addresses

--- common/chain_network.go
+++ common/chain_network.go
@@ -27,3 +27,14 @@ func GetCurrentChainNetwork() ChainNetwork {
 	}
 	return MainNet
 }
+
+// Soft Equals check is mainnet == mainet, or (testnet/mocknet == testnet/mocknet)
+func (net ChainNetwork) SoftEquals(net2 ChainNetwork) bool {
+	if net == MainNet && net2 == MainNet {
+		return true
+	}
+	if net != MainNet && net2 != MainNet {
+		return true
+	}
+	return false
+}
