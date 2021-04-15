19cecfb66342a4ba8a0a02cc9b7d2197505d4e9e
Heimdall <5053194-heimdallthor@users.noreply.gitlab.com>
2021-04-15 06:51:28 +0000

    Verify the destination address and destination chain match with each other

--- x/thorchain/managers.go
+++ x/thorchain/managers.go
@@ -276,7 +276,9 @@ func GetSwapQueue(keeper keeper.Keeper, version semver.Version) (SwapQueue, erro
 
 // GetSlasher return an implementation of Slasher
 func GetSlasher(keeper keeper.Keeper, version semver.Version, eventMgr EventManager) (Slasher, error) {
-	if version.GTE(semver.MustParse("0.1.0")) {
+	if version.GTE(semver.MustParse("0.43.0")) {
+		return NewSlasherV43(keeper, eventMgr), nil
+	} else if version.GTE(semver.MustParse("0.1.0")) {
 		return NewSlasherV1(keeper, eventMgr), nil
 	}
 	return nil, errInvalidVersion
