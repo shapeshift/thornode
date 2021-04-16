7d9bed33e55a91df71d344a83cf835cf9795c4c2
Son of Odin <sonofodin-thorchain@protonmail.com>
2021-04-15 23:24:38 -0400

    [bugfix] blank errata transaction

--- x/thorchain/handler_errata_tx.go
+++ x/thorchain/handler_errata_tx.go
@@ -64,7 +64,9 @@ func (h ErrataTxHandler) validateCurrent(ctx cosmos.Context, msg MsgErrataTx) er
 
 func (h ErrataTxHandler) handle(ctx cosmos.Context, msg MsgErrataTx, version semver.Version, constAccessor constants.ConstantValues) (*cosmos.Result, error) {
 	ctx.Logger().Info("handleMsgErrataTx request", "txid", msg.TxID.String())
-	if version.GTE(semver.MustParse("0.42.0")) {
+	if version.GTE(semver.MustParse("0.45.0")) {
+		return h.handleV45(ctx, msg, version, constAccessor)
+	} else if version.GTE(semver.MustParse("0.42.0")) {
 		return h.handleV42(ctx, msg, version, constAccessor)
 	} else if version.GTE(semver.MustParse("0.1.0")) {
 		return h.handleV1(ctx, msg, version, constAccessor)
@@ -199,10 +201,6 @@ func (h ErrataTxHandler) handleV1(ctx cosmos.Context, msg MsgErrataTx, version s
 }
 
 func (h ErrataTxHandler) handleV42(ctx cosmos.Context, msg MsgErrataTx, version semver.Version, constAccessor constants.ConstantValues) (*cosmos.Result, error) {
-	return h.handleCurrent(ctx, msg, version, constAccessor)
-}
-
-func (h ErrataTxHandler) handleCurrent(ctx cosmos.Context, msg MsgErrataTx, version semver.Version, constAccessor constants.ConstantValues) (*cosmos.Result, error) {
 	active, err := h.keeper.ListActiveNodeAccounts(ctx)
 	if err != nil {
 		return nil, wrapError(ctx, err, "fail to get list of active node accounts")
@@ -304,6 +302,150 @@ func (h ErrataTxHandler) handleCurrent(ctx cosmos.Context, msg MsgErrataTx, vers
 	pool.BalanceAsset = common.SafeSub(pool.BalanceAsset, assetCoin.Amount)
 	if memo.IsType(TxAdd) {
 		lp, err := h.keeper.GetLiquidityProvider(ctx, memo.GetAsset(), tx.FromAddress)
+		if err != nil {
+			return nil, fmt.Errorf("fail to get liquidity provider: %w", err)
+		}
+
+		// since this address is being malicious, zero their liquidity provider units
+		pool.PoolUnits = common.SafeSub(pool.PoolUnits, lp.Units)
+		lp.Units = cosmos.ZeroUint()
+		lp.LastAddHeight = common.BlockHeight(ctx)
+
+		h.keeper.SetLiquidityProvider(ctx, lp)
+	}
+
+	if err := h.keeper.SetPool(ctx, pool); err != nil {
+		ctx.Logger().Error("fail to save pool", "error", err)
+	}
+
+	// send errata event
+	mods := PoolMods{
+		NewPoolMod(pool.Asset, runeCoin.Amount, false, assetCoin.Amount, false),
+	}
+
+	eventErrata := NewEventErrata(msg.TxID, mods)
+	if err := h.mgr.EventMgr().EmitEvent(ctx, eventErrata); err != nil {
+		return nil, ErrInternal(err, "fail to emit errata event")
+	}
+	return &cosmos.Result{}, nil
+
+}
+
+func (h ErrataTxHandler) handleV45(ctx cosmos.Context, msg MsgErrataTx, version semver.Version, constAccessor constants.ConstantValues) (*cosmos.Result, error) {
+	return h.handleCurrent(ctx, msg, version, constAccessor)
+}
+
+func (h ErrataTxHandler) handleCurrent(ctx cosmos.Context, msg MsgErrataTx, version semver.Version, constAccessor constants.ConstantValues) (*cosmos.Result, error) {
+	active, err := h.keeper.ListActiveNodeAccounts(ctx)
+	if err != nil {
+		return nil, wrapError(ctx, err, "fail to get list of active node accounts")
+	}
+
+	voter, err := h.keeper.GetErrataTxVoter(ctx, msg.TxID, msg.Chain)
+	if err != nil {
+		return nil, err
+	}
+	observeSlashPoints := constAccessor.GetInt64Value(constants.ObserveSlashPoints)
+	observeFlex := constAccessor.GetInt64Value(constants.ObservationDelayFlexibility)
+	h.mgr.Slasher().IncSlashPoints(ctx, observeSlashPoints, msg.Signer)
+	if !voter.Sign(msg.Signer) {
+		ctx.Logger().Info("signer already signed MsgErrataTx", "signer", msg.Signer.String(), "txid", msg.TxID)
+		return &cosmos.Result{}, nil
+	}
+	h.keeper.SetErrataTxVoter(ctx, voter)
+	// doesn't have consensus yet
+	if !voter.HasConsensus(active) {
+		ctx.Logger().Info("not having consensus yet, return")
+		return &cosmos.Result{}, nil
+	}
+
+	if voter.BlockHeight > 0 {
+		if (voter.BlockHeight + observeFlex) >= common.BlockHeight(ctx) {
+			h.mgr.Slasher().DecSlashPoints(ctx, observeSlashPoints, msg.Signer)
+		}
+		// errata tx already processed
+		return &cosmos.Result{}, nil
+	}
+
+	voter.BlockHeight = common.BlockHeight(ctx)
+	h.keeper.SetErrataTxVoter(ctx, voter)
+	// decrease the slash points
+	h.mgr.Slasher().DecSlashPoints(ctx, observeSlashPoints, voter.GetSigners()...)
+	observedVoter, err := h.keeper.GetObservedTxInVoter(ctx, msg.TxID)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(observedVoter.Txs) == 0 {
+		return h.processErrataOutboundTxV42(ctx, msg)
+	}
+	// set the observed Tx to reverted
+	observedVoter.SetReverted()
+	h.keeper.SetObservedTxInVoter(ctx, observedVoter)
+	if observedVoter.Tx.IsEmpty() || !observedVoter.Tx.IsFinal() {
+		ctx.Logger().Info("tx is not finalised, so nothing need to be done", "tx_id", msg.TxID)
+		return &cosmos.Result{}, nil
+	}
+
+	tx := observedVoter.Tx.Tx
+	if !tx.Chain.Equals(msg.Chain) {
+		// does not match chain
+		return &cosmos.Result{}, nil
+	}
+	if observedVoter.UpdatedVault {
+		vaultPubKey := observedVoter.Tx.ObservedPubKey
+		if !vaultPubKey.IsEmpty() {
+			// try to deduct the asset from asgard
+			vault, err := h.keeper.GetVault(ctx, vaultPubKey)
+			if err != nil {
+				return nil, fmt.Errorf("fail to get active asgard vaults: %w", err)
+			}
+			vault.SubFunds(tx.Coins)
+			if err := h.keeper.SetVault(ctx, vault); err != nil {
+				return nil, fmt.Errorf("fail to save vault, err: %w", err)
+			}
+		}
+	}
+
+	memo, _ := ParseMemo(tx.Memo)
+	// if the tx is a migration , from old valut to new vault , then the inbound tx must have a related outbound tx as well
+	if memo.IsInternal() {
+		return h.processErrataOutboundTxV42(ctx, msg)
+	}
+
+	if !memo.IsType(TxSwap) && !memo.IsType(TxAdd) {
+		// must be a swap or add transaction
+		return &cosmos.Result{}, nil
+	}
+
+	runeCoin := common.NoCoin
+	assetCoin := common.NoCoin
+	for _, coin := range tx.Coins {
+		if coin.Asset.IsRune() {
+			runeCoin = coin
+		} else {
+			assetCoin = coin
+		}
+	}
+
+	// fetch pool from memo
+	pool, err := h.keeper.GetPool(ctx, assetCoin.Asset)
+	if err != nil {
+		ctx.Logger().Error("fail to get pool for errata tx", "error", err)
+		return nil, err
+	}
+
+	// subtract amounts from pool balances
+	if runeCoin.Amount.GT(pool.BalanceRune) {
+		runeCoin.Amount = pool.BalanceRune
+	}
+	if assetCoin.Amount.GT(pool.BalanceAsset) {
+		assetCoin.Amount = pool.BalanceAsset
+	}
+	pool.BalanceRune = common.SafeSub(pool.BalanceRune, runeCoin.Amount)
+	pool.BalanceAsset = common.SafeSub(pool.BalanceAsset, assetCoin.Amount)
+	if memo.IsType(TxAdd) {
+		lp, err := h.keeper.GetLiquidityProvider(ctx, pool.Asset, tx.FromAddress)
 		if err != nil {
 			return nil, fmt.Errorf("fail to get liquidity provider: %w", err)
 		}
