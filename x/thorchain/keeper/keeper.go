9d03aed93471823cd67100d914d001096134ac44
Heimdall <heimdallthor02@gmail.com>
2021-04-22 15:13:37 +1000

    Fix the issue that fee rate is not part of the consensus

--- x/thorchain/keeper/keeper.go
+++ x/thorchain/keeper/keeper.go
@@ -233,6 +233,7 @@ type KeeperObservedNetworkFeeVoter interface {
 	SetObservedNetworkFeeVoter(ctx cosmos.Context, networkFeeVoter ObservedNetworkFeeVoter)
 	GetObservedNetworkFeeVoterIterator(ctx cosmos.Context) cosmos.Iterator
 	GetObservedNetworkFeeVoter(ctx cosmos.Context, height int64, chain common.Chain) (ObservedNetworkFeeVoter, error)
+	GetObservedNetworkFeeVoterV47(ctx cosmos.Context, height int64, chain common.Chain, rate int64) (ObservedNetworkFeeVoter, error)
 }
 
 type KeeperChainContract interface {
