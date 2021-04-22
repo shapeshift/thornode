6380bfb9b55b6f5dab86f96f6d3fad0a5020591e
Heimdall <heimdallthor02@gmail.com>
2021-04-22 15:13:37 +1000

    Fix the issue that fee rate is not part of the consensus

--- x/thorchain/keeper/keeper_dummy.go
+++ x/thorchain/keeper/keeper_dummy.go
@@ -349,6 +349,9 @@ func (k KVStoreDummy) GetObservedNetworkFeeVoterIterator(ctx cosmos.Context) cos
 func (k KVStoreDummy) GetObservedNetworkFeeVoter(ctx cosmos.Context, height int64, chain common.Chain) (ObservedNetworkFeeVoter, error) {
 	return ObservedNetworkFeeVoter{}, nil
 }
+func (k KVStoreDummy) GetObservedNetworkFeeVoterV47(ctx cosmos.Context, height int64, chain common.Chain, rate int64) (ObservedNetworkFeeVoter, error) {
+	return ObservedNetworkFeeVoter{}, nil
+}
 
 func (k KVStoreDummy) SetLastObserveHeight(ctx cosmos.Context, chain common.Chain, address cosmos.AccAddress, height int64) error {
 	return kaboom
