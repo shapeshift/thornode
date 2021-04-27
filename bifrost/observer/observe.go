1b76a8e3a4e30bc9c5fc78d89860a0f4a6cbc306
Son of Odin <sonofodin-thorchain@protonmail.com>
2021-04-27 11:08:56 -0400

    recover memo parsing panic

--- bifrost/observer/observe.go
+++ bifrost/observer/observe.go
@@ -313,15 +313,7 @@ func (o *Observer) filterBinanceMemoFlag(chain common.Chain, items []types.TxInI
 	}
 	for _, txInItem := range items {
 		var addressesToCheck []string
-		addr, err := txInItem.GetAddressToCheck()
-		if err != nil {
-			o.logger.Error().Err(err).Msgf("fail to get address to check")
-			if errors.Is(err, types.ErrPanicParseMemo) {
-				// override the memo as it can't be parsed
-				// so THORNode can refund it
-				txInItem.Memo = "invalid memo"
-			}
-		}
+		addr := txInItem.GetAddressToCheck()
 		if !addr.IsEmpty() && addr.IsChain(common.BNBChain) {
 			addressesToCheck = append(addressesToCheck, addr.String())
 		}
