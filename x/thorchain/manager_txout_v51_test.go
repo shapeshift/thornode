49dd5e70a04426a724b1160056892a432347becb
Heimdall <heimdallthor02@gmail.com>
2021-05-05 22:00:50 +1000

    add unit tests

--- x/thorchain/manager_txout_v51_test.go
+++ x/thorchain/manager_txout_v51_test.go
@@ -4,7 +4,7 @@ import (
 	. "gopkg.in/check.v1"
 
 	"gitlab.com/thorchain/thornode/common"
-	cosmos "gitlab.com/thorchain/thornode/common/cosmos"
+	"gitlab.com/thorchain/thornode/common/cosmos"
 	"gitlab.com/thorchain/thornode/constants"
 	"gitlab.com/thorchain/thornode/x/thorchain/types"
 )
@@ -223,4 +223,94 @@ func (s TxOutStoreV51Suite) TestAddOutTxItemWithoutBFT(c *C) {
 	c.Assert(err, IsNil)
 	c.Assert(msgs, HasLen, 1)
 	c.Assert(msgs[0].Coin.Amount.Equal(cosmos.NewUint(1999887500)), Equals, true, Commentf("%d", msgs[0].Coin.Amount.Uint64()))
+}
+
+func (s TxOutStoreV51Suite) TestAddOutTxItemDeductMaxGasFromYggdrasil(c *C) {
+	w := getHandlerTestWrapper(c, 1, true, true)
+	vault := GetRandomVault()
+	vault.Coins = common.Coins{
+		common.NewCoin(common.RuneAsset(), cosmos.NewUint(10000*common.One)),
+		common.NewCoin(common.BNBAsset, cosmos.NewUint(10000*common.One)),
+	}
+	w.keeper.SetVault(w.ctx, vault)
+
+	acc1 := GetRandomNodeAccount(NodeActive)
+	acc2 := GetRandomNodeAccount(NodeActive)
+	acc3 := GetRandomNodeAccount(NodeActive)
+	c.Assert(w.keeper.SetNodeAccount(w.ctx, acc1), IsNil)
+	c.Assert(w.keeper.SetNodeAccount(w.ctx, acc2), IsNil)
+	c.Assert(w.keeper.SetNodeAccount(w.ctx, acc3), IsNil)
+
+	ygg := NewVault(common.BlockHeight(w.ctx), ActiveVault, YggdrasilVault, acc1.PubKeySet.Secp256k1, common.Chains{common.BNBChain}.Strings(), []ChainContract{})
+	ygg.AddFunds(
+		common.Coins{
+			common.NewCoin(common.BNBAsset, cosmos.NewUint(11*common.One)),
+		},
+	)
+	c.Assert(w.keeper.SetVault(w.ctx, ygg), IsNil)
+
+	ygg = NewVault(common.BlockHeight(w.ctx), ActiveVault, YggdrasilVault, acc2.PubKeySet.Secp256k1, common.Chains{common.BNBChain}.Strings(), []ChainContract{})
+	ygg.AddFunds(
+		common.Coins{
+			common.NewCoin(common.BNBAsset, cosmos.NewUint(50*common.One)),
+		},
+	)
+	c.Assert(w.keeper.SetVault(w.ctx, ygg), IsNil)
+
+	ygg = NewVault(common.BlockHeight(w.ctx), ActiveVault, YggdrasilVault, acc3.PubKeySet.Secp256k1, common.Chains{common.BNBChain}.Strings(), []ChainContract{})
+	ygg.AddFunds(
+		common.Coins{
+			common.NewCoin(common.BNBAsset, cosmos.NewUint(100*common.One)),
+		},
+	)
+	c.Assert(w.keeper.SetVault(w.ctx, ygg), IsNil)
+
+	// Create voter
+	inTxID := GetRandomTxHash()
+	voter := NewObservedTxVoter(inTxID, ObservedTxs{
+		ObservedTx{
+			Tx:             GetRandomTx(),
+			Status:         types.Status_incomplete,
+			BlockHeight:    1,
+			Signers:        []string{w.activeNodeAccount.NodeAddress.String(), acc1.NodeAddress.String(), acc2.NodeAddress.String()},
+			KeysignMs:      0,
+			FinaliseHeight: 1,
+		},
+	})
+	w.keeper.SetObservedTxInVoter(w.ctx, voter)
+
+	// Should get acc2. Acc3 hasn't signed and acc2 is the highest value
+	item := TxOutItem{
+		Chain:     common.BNBChain,
+		ToAddress: GetRandomBNBAddress(),
+		InHash:    inTxID,
+		Coin:      common.NewCoin(common.BNBAsset, cosmos.NewUint(3900000000)),
+		MaxGas: common.Gas{
+			common.NewCoin(common.BNBAsset, cosmos.NewUint(100000000)),
+		},
+	}
+	txOutStore := w.mgr.TxOutStore()
+	ok, err := txOutStore.TryAddTxOutItem(w.ctx, w.mgr, item)
+	c.Assert(err, IsNil)
+	c.Assert(ok, Equals, true)
+	msgs, err := txOutStore.GetOutboundItems(w.ctx)
+	c.Assert(err, IsNil)
+	c.Assert(msgs, HasLen, 1)
+
+	item1 := TxOutItem{
+		Chain:     common.BNBChain,
+		ToAddress: GetRandomBNBAddress(),
+		InHash:    inTxID,
+		Coin:      common.NewCoin(common.BNBAsset, cosmos.NewUint(1000000000)),
+		MaxGas: common.Gas{
+			common.NewCoin(common.BNBAsset, cosmos.NewUint(7500)),
+		},
+	}
+	ok, err = txOutStore.TryAddTxOutItem(w.ctx, w.mgr, item1)
+	c.Assert(err, IsNil)
+	c.Assert(ok, Equals, true)
+	msgs, err = txOutStore.GetOutboundItems(w.ctx)
+	c.Assert(err, IsNil)
+	c.Assert(msgs, HasLen, 2)
+	c.Assert(msgs[1].VaultPubKey.Equals(acc1.PubKeySet.Secp256k1), Equals, true)
 }
