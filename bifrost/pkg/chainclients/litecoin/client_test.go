6f02c965db7dc855b1b75d927e2770d3e721070d
Heimdall <5053194-heimdallthor@users.noreply.gitlab.com>
2021-05-25 05:16:33 +0000

    Update confirmation counting logic

--- bifrost/pkg/chainclients/litecoin/client_test.go
+++ bifrost/pkg/chainclients/litecoin/client_test.go
@@ -759,3 +759,112 @@ func (s *LitecoinSuite) TestConfirmationCountReady(c *C) {
 		ConfirmationRequired: 5,
 	}), Equals, false)
 }
+
+func (s *LitecoinSuite) TestGetConfirmationCount(c *C) {
+	pkey := ttypes.GetRandomPubKey()
+	// no tx in item , confirmation count should be 0
+	c.Assert(s.client.GetConfirmationCount(types.TxIn{
+		Chain:   common.LTCChain,
+		TxArray: nil,
+	}), Equals, int64(0))
+	// mempool txin , confirmation count should be 0
+	c.Assert(s.client.GetConfirmationCount(types.TxIn{
+		Chain: common.BTCChain,
+		TxArray: []types.TxInItem{
+			{
+				BlockHeight: 2,
+				Tx:          "24ed2d26fd5d4e0e8fa86633e40faf1bdfc8d1903b1cd02855286312d48818a2",
+				Sender:      "ltc1q2gjc0rnhy4nrxvuklk6ptwkcs9kcr59mursyaz",
+				To:          "ltc1qjw8h4l3dtz5xxc7uyh5ys70qkezspgfu8hg5j3",
+				Coins: common.Coins{
+					common.NewCoin(common.LTCAsset, cosmos.NewUint(123456)),
+				},
+				Memo:                "MEMO",
+				ObservedVaultPubKey: pkey,
+			},
+		},
+		Filtered:             true,
+		MemPool:              true,
+		ConfirmationRequired: 0,
+	}), Equals, int64(0))
+
+	c.Assert(s.client.GetConfirmationCount(types.TxIn{
+		Chain: common.LTCChain,
+		TxArray: []types.TxInItem{
+			{
+				BlockHeight: 2,
+				Tx:          "24ed2d26fd5d4e0e8fa86633e40faf1bdfc8d1903b1cd02855286312d48818a2",
+				Sender:      "ltc1q2gjc0rnhy4nrxvuklk6ptwkcs9kcr59mursyaz",
+				To:          "ltc1qjw8h4l3dtz5xxc7uyh5ys70qkezspgfu8hg5j3",
+				Coins: common.Coins{
+					common.NewCoin(common.LTCAsset, cosmos.NewUint(123456)),
+				},
+				Memo:                "MEMO",
+				ObservedVaultPubKey: pkey,
+			},
+		},
+		Filtered:             true,
+		MemPool:              false,
+		ConfirmationRequired: 0,
+	}), Equals, int64(0))
+
+	c.Assert(s.client.GetConfirmationCount(types.TxIn{
+		Chain: common.LTCChain,
+		TxArray: []types.TxInItem{
+			{
+				BlockHeight: 2,
+				Tx:          "24ed2d26fd5d4e0e8fa86633e40faf1bdfc8d1903b1cd02855286312d48818a2",
+				Sender:      "bc1q0s4mg25tu6termrk8egltfyme4q7sg3h0e56p3",
+				To:          "bc1q2gjc0rnhy4nrxvuklk6ptwkcs9kcr59mcl2q9j",
+				Coins: common.Coins{
+					common.NewCoin(common.LTCAsset, cosmos.NewUint(12345600)),
+				},
+				Memo:                "MEMO",
+				ObservedVaultPubKey: pkey,
+			},
+		},
+		Filtered:             true,
+		MemPool:              false,
+		ConfirmationRequired: 0,
+	}), Equals, int64(0))
+
+	c.Assert(s.client.GetConfirmationCount(types.TxIn{
+		Chain: common.LTCChain,
+		TxArray: []types.TxInItem{
+			{
+				BlockHeight: 2,
+				Tx:          "24ed2d26fd5d4e0e8fa86633e40faf1bdfc8d1903b1cd02855286312d48818a2",
+				Sender:      "ltc1q2gjc0rnhy4nrxvuklk6ptwkcs9kcr59mursyaz",
+				To:          "ltc1qjw8h4l3dtz5xxc7uyh5ys70qkezspgfu8hg5j3",
+				Coins: common.Coins{
+					common.NewCoin(common.LTCAsset, cosmos.NewUint(22345600)),
+				},
+				Memo:                "MEMO",
+				ObservedVaultPubKey: pkey,
+			},
+		},
+		Filtered:             true,
+		MemPool:              false,
+		ConfirmationRequired: 0,
+	}), Equals, int64(1))
+
+	c.Assert(s.client.GetConfirmationCount(types.TxIn{
+		Chain: common.LTCChain,
+		TxArray: []types.TxInItem{
+			{
+				BlockHeight: 2,
+				Tx:          "24ed2d26fd5d4e0e8fa86633e40faf1bdfc8d1903b1cd02855286312d48818a2",
+				Sender:      "ltc1q2gjc0rnhy4nrxvuklk6ptwkcs9kcr59mursyaz",
+				To:          "ltc1qjw8h4l3dtz5xxc7uyh5ys70qkezspgfu8hg5j3",
+				Coins: common.Coins{
+					common.NewCoin(common.LTCAsset, cosmos.NewUint(123456000)),
+				},
+				Memo:                "MEMO",
+				ObservedVaultPubKey: pkey,
+			},
+		},
+		Filtered:             true,
+		MemPool:              false,
+		ConfirmationRequired: 0,
+	}), Equals, int64(6))
+}
