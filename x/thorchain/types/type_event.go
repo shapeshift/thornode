d46105c6be7011f1cb663b03fda2411c4963c1c8
Son of Odin <sonofodin-thorchain@protonmail.com>
2021-06-14 08:58:48 -0400

    [bugfix] add tx hash to switch event
    tweak

--- x/thorchain/types/type_event.go
+++ x/thorchain/types/type_event.go
@@ -556,6 +556,31 @@ func (m *EventSwitch) Events() (cosmos.Events, error) {
 	return cosmos.Events{evt}, nil
 }
 
+// NewEventSwitchV56 create a new instance of EventSwitch
+func NewEventSwitchV56(from common.Address, to cosmos.AccAddress, coin common.Coin, hash common.TxID) *EventSwitchV56 {
+	return &EventSwitchV56{
+		TxID:        hash,
+		ToAddress:   to,
+		FromAddress: from,
+		Burn:        coin,
+	}
+}
+
+// Type return a string which represent the type of this event
+func (m *EventSwitchV56) Type() string {
+	return SwitchEventType
+}
+
+// Events return cosmos sdk events
+func (m *EventSwitchV56) Events() (cosmos.Events, error) {
+	evt := cosmos.NewEvent(m.Type(),
+		cosmos.NewAttribute("txid", m.TxID.String()),
+		cosmos.NewAttribute("from", m.FromAddress.String()),
+		cosmos.NewAttribute("to", m.ToAddress.String()),
+		cosmos.NewAttribute("burn", m.Burn.String()))
+	return cosmos.Events{evt}, nil
+}
+
 // NewEventPendingLiquidity create a new add pending liquidity event
 func NewEventPendingLiquidity(pool common.Asset,
 	ptype PendingLiquidityType,
