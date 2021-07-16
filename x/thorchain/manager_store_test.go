bccb2a9fa87e7ab496b3ef9729eb9e3cd911265a
Heimdall <5053194-heimdallthor@users.noreply.gitlab.com>
2021-07-16 05:56:32 +0000

    Resolve "[BUG] Fix ETH chain attack"

--- x/thorchain/manager_store_test.go
+++ x/thorchain/manager_store_test.go
@@ -193,3 +193,166 @@ func (s *StoreManagerTestSuite) TestMigrateStoreV58Refund(c *C) {
 	txAfter1 := voterAfter.GetTx(NodeAccounts{})
 	c.Assert(txAfter1.IsDone(len(voterAfter1.Actions)), Equals, true)
 }
+
+func (s *StoreManagerTestSuite) TestRemoveTransactions(c *C) {
+	ctx, mgr := setupManagerForTest(c)
+	storeMgr := NewStoreMgr(mgr)
+	vault := NewVault(1024, ActiveVault, AsgardVault, GetRandomPubKey(), []string{
+		common.ETHChain.String(),
+	}, nil)
+
+	c.Assert(storeMgr.mgr.Keeper().SaveNetworkFee(ctx, common.ETHChain, NetworkFee{
+		common.ETHChain, 80000, 30,
+	}), IsNil)
+
+	inTxID, err := common.NewTxID("BC68035CE2C8A2C549604FF7DB59E07931F39040758B138190338FA697338DB3")
+	c.Assert(err, IsNil)
+	tx := common.NewTx(inTxID,
+		"0x3a196410a0f5facd08fd7880a4b8551cd085c031",
+		"0xf56cBa49337A624E94042e325Ad6Bc864436E370",
+		common.NewCoins(common.NewCoin(common.ETHAsset, cosmos.NewUint(200*common.One))),
+		common.Gas{
+			common.NewCoin(common.RuneNative, cosmos.NewUint(2000000)),
+		}, "SWAP:ETH.AAVE-0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9")
+	observedTx := NewObservedTx(tx, 1281323, vault.PubKey, 1281323)
+	voter := NewObservedTxVoter(inTxID, []ObservedTx{
+		observedTx,
+	})
+	aaveAsset, _ := common.NewAsset("ETH.AAVE-0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9")
+	voter.Actions = []TxOutItem{
+		{
+			Chain:       common.ETHChain,
+			ToAddress:   "0x3a196410a0f5facd08fd7880a4b8551cd085c031",
+			VaultPubKey: vault.PubKey,
+			Coin:        common.NewCoin(aaveAsset, cosmos.NewUint(1422136902)),
+			Memo:        "OUT:BC68035CE2C8A2C549604FF7DB59E07931F39040758B138190338FA697338DB3",
+			InHash:      "BC68035CE2C8A2C549604FF7DB59E07931F39040758B138190338FA697338DB3",
+		},
+		{
+			Chain:       common.ETHChain,
+			ToAddress:   "0x3a196410a0f5facd08fd7880a4b8551cd085c031",
+			VaultPubKey: vault.PubKey,
+			Coin:        common.NewCoin(aaveAsset, cosmos.NewUint(1330195098)),
+			Memo:        "OUT:BC68035CE2C8A2C549604FF7DB59E07931F39040758B138190338FA697338DB3",
+			InHash:      "BC68035CE2C8A2C549604FF7DB59E07931F39040758B138190338FA697338DB3",
+		},
+	}
+	voter.Tx = voter.Txs[0]
+	storeMgr.mgr.Keeper().SetObservedTxInVoter(ctx, voter)
+	hegicAsset, _ := common.NewAsset("ETH.HEGIC-0x584bC13c7D411c00c01A62e8019472dE68768430")
+	inTxID1, err := common.NewTxID("5DA19C1C5C2F6BBDF9D4FB0E6FF16A0DF6D6D7FE1F8E95CA755E5B3C6AADA458")
+	c.Assert(err, IsNil)
+	tx1 := common.NewTx(inTxID1,
+		"0x3a196410a0f5facd08fd7880a4b8551cd085c031",
+		"0xf56cBa49337A624E94042e325Ad6Bc864436E370",
+		common.NewCoins(common.NewCoin(common.ETHAsset, cosmos.NewUint(200*common.One))),
+		common.Gas{
+			common.NewCoin(common.RuneNative, cosmos.NewUint(2000000)),
+		}, "SWAP:ETH.HEGIC-0x584bC13c7D411c00c01A62e8019472dE68768430")
+	observedTx1 := NewObservedTx(tx1, 1281323, vault.PubKey, 1281323)
+	voter1 := NewObservedTxVoter(inTxID1, []ObservedTx{
+		observedTx1,
+	})
+	voter1.Actions = []TxOutItem{
+		{
+			Chain:       common.ETHChain,
+			ToAddress:   "0x3a196410a0f5facd08fd7880a4b8551cd085c031",
+			VaultPubKey: vault.PubKey,
+			Coin:        common.NewCoin(hegicAsset, cosmos.NewUint(3083783295390)),
+			Memo:        "OUT:5DA19C1C5C2F6BBDF9D4FB0E6FF16A0DF6D6D7FE1F8E95CA755E5B3C6AADA458",
+			InHash:      "5DA19C1C5C2F6BBDF9D4FB0E6FF16A0DF6D6D7FE1F8E95CA755E5B3C6AADA458",
+		},
+		{
+			Chain:       common.ETHChain,
+			ToAddress:   "0x3a196410a0f5facd08fd7880a4b8551cd085c031",
+			VaultPubKey: vault.PubKey,
+			Coin:        common.NewCoin(hegicAsset, cosmos.NewUint(2481151780248)),
+			Memo:        "OUT:5DA19C1C5C2F6BBDF9D4FB0E6FF16A0DF6D6D7FE1F8E95CA755E5B3C6AADA458",
+			InHash:      "5DA19C1C5C2F6BBDF9D4FB0E6FF16A0DF6D6D7FE1F8E95CA755E5B3C6AADA458",
+		},
+	}
+	voter1.Tx = voter.Txs[0]
+	storeMgr.mgr.Keeper().SetObservedTxInVoter(ctx, voter1)
+
+	inTxID2, err := common.NewTxID("D58D1EF6D6E49EB99D0524128C16115893396FD05877EF4856FCE474B5BA09A7")
+	c.Assert(err, IsNil)
+	tx2 := common.NewTx(inTxID2,
+		"0x3a196410a0f5facd08fd7880a4b8551cd085c031",
+		"0xf56cBa49337A624E94042e325Ad6Bc864436E370",
+		common.NewCoins(common.NewCoin(common.ETHAsset, cosmos.NewUint(150005145000))),
+		common.Gas{
+			common.NewCoin(common.RuneNative, cosmos.NewUint(2000000)),
+		}, "SWAP:ETH.ETH")
+	observedTx2 := NewObservedTx(tx2, 1281323, vault.PubKey, 1281323)
+	voter2 := NewObservedTxVoter(inTxID2, []ObservedTx{
+		observedTx2,
+	})
+	voter2.Actions = []TxOutItem{
+		{
+			Chain:       common.ETHChain,
+			ToAddress:   "0x3a196410a0f5facd08fd7880a4b8551cd085c031",
+			VaultPubKey: vault.PubKey,
+			Coin:        common.NewCoin(hegicAsset, cosmos.NewUint(150003465000)),
+			Memo:        "REFUND:D58D1EF6D6E49EB99D0524128C16115893396FD05877EF4856FCE474B5BA09A7",
+			InHash:      "D58D1EF6D6E49EB99D0524128C16115893396FD05877EF4856FCE474B5BA09A7",
+		},
+	}
+	voter2.Tx = voter.Txs[0]
+	storeMgr.mgr.Keeper().SetObservedTxInVoter(ctx, voter2)
+
+	allTxIDs := []common.TxID{
+		inTxID, inTxID1, inTxID2,
+	}
+	storeMgr.removeTransactions(ctx, inTxID.String(), inTxID1.String(), inTxID2.String())
+	for _, txID := range allTxIDs {
+		voterAfter, err := storeMgr.mgr.Keeper().GetObservedTxInVoter(ctx, txID)
+		c.Assert(err, IsNil)
+		txAfter := voterAfter.GetTx(NodeAccounts{})
+		c.Assert(txAfter.IsDone(len(voterAfter.Actions)), Equals, true)
+	}
+}
+func (s *StoreManagerTestSuite) TestCreditBackToVaultAndPool(c *C) {
+	ctx, mgr := setupManagerForTest(c)
+	storeMgr := NewStoreMgr(mgr)
+	vault := NewVault(1024, ActiveVault, AsgardVault, GetRandomPubKey(), []string{
+		common.ETHChain.String(),
+	}, nil)
+	vault.AddFunds(common.NewCoins(
+		common.NewCoin(common.ETHAsset, cosmos.NewUint(1339348406651)),
+	))
+	c.Assert(storeMgr.mgr.Keeper().SetVault(ctx, vault), IsNil)
+	c.Assert(storeMgr.mgr.Keeper().SaveNetworkFee(ctx, common.ETHChain, NetworkFee{
+		common.ETHChain, 80000, 30,
+	}), IsNil)
+
+	assetsForTest := map[string][2]cosmos.Uint{
+		"ETH.ETH": {cosmos.NewUint(1006103676112), cosmos.NewUint(70306321826847)},
+		"ETH.AAVE-0X7FC66500C84A76AD7E9C93437BFC5AC33E2DDAE9":  {cosmos.NewUint(64528597541), cosmos.NewUint(8356937728557)},
+		"ETH.HEGIC-0X584BC13C7D411C00C01A62E8019472DE68768430": {cosmos.NewUint(115332831324359), cosmos.NewUint(5823770298376)},
+		"ETH.YFI-0X0BC529C00C6401AEF6D220BE8C6EA1667F6AD93E":   {cosmos.NewUint(837597381), cosmos.NewUint(43408026406467)},
+		"ETH.SUSHI-0X6B3595068778DD592E39A122F4F5A5CF09C90FE2": {cosmos.NewUint(6227149236123), cosmos.NewUint(47444039861851)},
+		"ETH.KYL-0X67B6D479C7BB412C54E03DCA8E1BC6740CE6B99C":   {cosmos.NewUint(244452777487314), cosmos.NewUint(17322656702203)},
+		"ETH.DODO-0X43DFC4159D86F3A37A5A4B3D4580B888AD7D4DDD":  {cosmos.NewUint(36302299741717), cosmos.NewUint(17608104987305)},
+	}
+
+	for k, item := range assetsForTest {
+		asset, err := common.NewAsset(k)
+		c.Assert(err, IsNil)
+		pool, err := mgr.Keeper().GetPool(ctx, asset)
+		c.Assert(err, IsNil)
+		pool.Asset = asset
+		pool.Status = PoolAvailable
+		pool.BalanceAsset = item[0]
+		pool.BalanceRune = item[1]
+		c.Assert(mgr.Keeper().SetPool(ctx, pool), IsNil)
+	}
+	storeMgr.creditAssetBackToVaultAndPool(ctx)
+
+	vaultAfter, err := mgr.Keeper().GetVault(ctx, vault.PubKey)
+	c.Assert(err, IsNil)
+	c.Assert(vaultAfter.GetCoin(common.ETHAsset).Amount.Equal(cosmos.NewUint(43465695171)), Equals, true)
+	ethPoolAfter, err := mgr.Keeper().GetPool(ctx, common.ETHAsset)
+	c.Assert(err, IsNil)
+	c.Assert(ethPoolAfter.BalanceAsset.Equal(cosmos.NewUint(76228226137)), Equals, true)
+	c.Assert(ethPoolAfter.BalanceRune.GT(cosmos.NewUint(70306321826847)), Equals, true)
+}
