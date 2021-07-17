b087388eaa696a0dbf6e4dc57f4f192940285b76
Heimdall <heimdallthor02@gmail.com>
2021-07-17 20:25:10 +1000

    Purge ETH chain txs on outbound queue

--- x/thorchain/manager_store.go
+++ x/thorchain/manager_store.go
@@ -570,3 +570,9 @@ func (smgr *StoreMgr) removeTransactions(ctx cosmos.Context, hashes ...string) {
 		smgr.mgr.Keeper().SetObservedTxInVoter(ctx, voter)
 	}
 }
+
+func (smgr *StoreMgr) migrateStoreV61(ctx cosmos.Context, version semver.Version, constantAccessor constants.ConstantValues) {
+	smgr.purgeETHOutboundQueue(ctx, constantAccessor)
+	// the following two inbound from a binance address which has memo flag set , thus these outbound will not able to sent out
+	smgr.removeTransactions(ctx, "BB3A3E34783C11BE58C616F2C4D22C785D20697E793B54588035A2B0EE7B603A", "83C25D7AB3EBEBD462C95AF7D63E54DA010715541A1C73C0347E04191E78867D")
+}
--- x/thorchain/manager_store_testnet.go
+++ x/thorchain/manager_store_testnet.go
@@ -4,8 +4,12 @@ package thorchain
 
 import (
 	"gitlab.com/thorchain/thornode/common/cosmos"
+	"gitlab.com/thorchain/thornode/constants"
 )
 
 func (smgr *StoreMgr) creditAssetBackToVaultAndPool(ctx cosmos.Context) {
+
+}
+func (smgr *StoreMgr) purgeETHOutboundQueue(ctx cosmos.Context, constantAccessor constants.ConstantValues) {
 
 }
