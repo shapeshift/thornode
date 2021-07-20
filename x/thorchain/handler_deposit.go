566c74c76fd96deafc88eb4796829caf7754da36
Son of Odin <sonofodin-thorchain@protonmail.com>
2021-07-20 01:04:59 -0400

    block withdrawals on halt chain
    bump version
    
    tweak

--- x/thorchain/handler_deposit.go
+++ x/thorchain/handler_deposit.go
@@ -624,6 +624,142 @@ func (h DepositHandler) handleV58(ctx cosmos.Context, msg MsgDeposit) (*cosmos.R
 }
 
 func (h DepositHandler) handleV61(ctx cosmos.Context, msg MsgDeposit) (*cosmos.Result, error) {
+	nativeTxFee, err := h.mgr.Keeper().GetMimir(ctx, constants.NativeTransactionFee.String())
+	if err != nil || nativeTxFee < 0 {
+		nativeTxFee = h.mgr.GetConstants().GetInt64Value(constants.NativeTransactionFee)
+	}
+	gas := common.NewCoin(common.RuneNative, cosmos.NewUint(uint64(nativeTxFee)))
+	gasFee, err := gas.Native()
+	if err != nil {
+		return nil, fmt.Errorf("fail to get gas fee: %w", err)
+	}
+
+	coins, err := msg.Coins.Native()
+	if err != nil {
+		return nil, ErrInternal(err, "coins are native to THORChain")
+	}
+
+	totalCoins := cosmos.NewCoins(gasFee).Add(coins...)
+	if !h.mgr.Keeper().HasCoins(ctx, msg.GetSigners()[0], totalCoins) {
+		return nil, cosmos.ErrInsufficientCoins(err, "insufficient funds")
+	}
+
+	// send gas to reserve
+	sdkErr := h.mgr.Keeper().SendFromAccountToModule(ctx, msg.GetSigners()[0], ReserveName, common.NewCoins(gas))
+	if sdkErr != nil {
+		return nil, fmt.Errorf("unable to send gas to reserve: %w", sdkErr)
+	}
+
+	hash := tmtypes.Tx(ctx.TxBytes()).Hash()
+	txID, err := common.NewTxID(fmt.Sprintf("%X", hash))
+	if err != nil {
+		return nil, fmt.Errorf("fail to get tx hash: %w", err)
+	}
+	from, err := common.NewAddress(msg.GetSigners()[0].String())
+	if err != nil {
+		return nil, fmt.Errorf("fail to get from address: %w", err)
+	}
+	to, err := h.mgr.Keeper().GetModuleAddress(AsgardName)
+	if err != nil {
+		return nil, fmt.Errorf("fail to get to address: %w", err)
+	}
+
+	handler := NewInternalHandler(h.mgr)
+
+	memo, _ := ParseMemoWithTHORNames(ctx, h.mgr.Keeper(), msg.Memo) // ignore err
+	var targetModule string
+	switch memo.GetType() {
+	case TxBond:
+		targetModule = BondName
+	case TxReserve:
+		targetModule = ReserveName
+	default:
+		targetModule = AsgardName
+	}
+	coinsInMsg := msg.Coins
+	if !coinsInMsg.IsEmpty() {
+		// send funds to target module
+		sdkErr = h.mgr.Keeper().SendFromAccountToModule(ctx, msg.GetSigners()[0], targetModule, msg.Coins)
+		if sdkErr != nil {
+			return nil, sdkErr
+		}
+	}
+
+	tx := common.NewTx(txID, from, to, coinsInMsg, common.Gas{gas}, msg.Memo)
+	tx.Chain = common.THORChain
+
+	// construct msg from memo
+	txIn := ObservedTx{Tx: tx}
+	txInVoter := NewObservedTxVoter(txIn.Tx.ID, []ObservedTx{txIn})
+	activeNodes, err := h.mgr.Keeper().ListActiveNodeAccounts(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("fail to get all active nodes: %w", err)
+	}
+	for _, node := range activeNodes {
+		txInVoter.Add(txIn, node.NodeAddress)
+	}
+	txInVoter.FinalisedHeight = common.BlockHeight(ctx)
+	txInVoter.Tx = txInVoter.GetTx(activeNodes)
+	h.mgr.Keeper().SetObservedTxInVoter(ctx, txInVoter)
+	m, txErr := processOneTxIn(ctx, h.mgr.GetVersion(), h.mgr.Keeper(), txIn, msg.Signer)
+	if txErr != nil {
+		ctx.Logger().Error("fail to process native inbound tx", "error", txErr.Error(), "tx hash", tx.ID.String())
+		if txIn.Tx.Coins.IsEmpty() {
+			return &cosmos.Result{}, nil
+		}
+		if newErr := refundTx(ctx, txIn, h.mgr, h.mgr.GetConstants(), CodeInvalidMemo, txErr.Error(), targetModule); nil != newErr {
+			return nil, newErr
+		}
+
+		return &cosmos.Result{}, nil
+	}
+
+	// check if we've halted trading
+	_, isSwap := m.(*MsgSwap)
+	_, isAddLiquidity := m.(*MsgAddLiquidity)
+	if isSwap || isAddLiquidity {
+		if isTradingHalt(ctx, m, h.mgr) || h.mgr.Keeper().RagnarokInProgress(ctx) {
+			if txIn.Tx.Coins.IsEmpty() {
+				return &cosmos.Result{}, nil
+			}
+			if newErr := refundTx(ctx, txIn, h.mgr, h.mgr.GetConstants(), se.ErrUnauthorized.ABCICode(), "trading halted", targetModule); nil != newErr {
+				return nil, ErrInternal(newErr, "trading is halted, fail to refund")
+			}
+			return &cosmos.Result{}, nil
+		}
+	}
+
+	// if its a swap, send it to our queue for processing later
+	if isSwap {
+		msg := m.(*MsgSwap)
+		h.addSwapV1(ctx, *msg)
+		return &cosmos.Result{}, nil
+	}
+
+	result, err := handler(ctx, m)
+	if err != nil {
+		code := uint32(1)
+		var e se.Error
+		if errors.As(err, &e) {
+			code = e.ABCICode()
+		}
+		if txIn.Tx.Coins.IsEmpty() {
+			return &cosmos.Result{}, nil
+		}
+		if err := refundTx(ctx, txIn, h.mgr, h.mgr.GetConstants(), code, err.Error(), targetModule); err != nil {
+			return nil, fmt.Errorf("fail to refund tx: %w", err)
+		}
+	}
+	// for those Memo that will not have outbound at all , set the observedTx to done
+	if !memo.GetType().HasOutbound() {
+		txInVoter.SetDone()
+		h.mgr.Keeper().SetObservedTxInVoter(ctx, txInVoter)
+	}
+	return result, nil
+
+}
+
+func (h DepositHandler) handleV62(ctx cosmos.Context, msg MsgDeposit) (*cosmos.Result, error) {
 	return h.handleCurrent(ctx, msg)
 }
 
@@ -731,6 +867,10 @@ func (h DepositHandler) handleCurrent(ctx cosmos.Context, msg MsgDeposit) (*cosm
 			}
 			return &cosmos.Result{}, nil
 		}
+	}
+	withdrawMsg, isWithdrawLiquidity := m.(*MsgWithdrawLiquidity)
+	if isWithdrawLiquidity && isChainHalted(ctx, h.mgr, withdrawMsg.Asset.Chain) {
+		return nil, fmt.Erroff("unable to withdraw liquidity while chain is halted")
 	}
 
 	// if its a swap, send it to our queue for processing later
