bde24e93da0d88c70d4ceaafc84f521c48d98a01
Heimdall <heimdallthor02@gmail.com>
2021-08-04 08:59:15 +1000

    apply change , only inbound UTXO will be validate against multi-sig

--- bifrost/pkg/chainclients/bitcoin/client.go
+++ bifrost/pkg/chainclients/bitcoin/client.go
@@ -289,7 +289,7 @@ func (c *Client) getAsgardAddress() ([]common.Address, error) {
 	return c.asgardAddresses, nil
 }
 
-func (c *Client) isFromAsgard(txIn types.TxInItem) bool {
+func (c *Client) isFromAsgard(fromAddr string) bool {
 	asgards, err := c.getAsgardAddress()
 	if err != nil {
 		c.logger.Err(err).Msg("fail to get asgard addresses")
@@ -297,7 +297,7 @@ func (c *Client) isFromAsgard(txIn types.TxInItem) bool {
 	}
 	isFromAsgard := false
 	for _, addr := range asgards {
-		if addr.String() == txIn.Sender {
+		if strings.EqualFold(addr.String(), fromAddr) {
 			isFromAsgard = true
 			break
 		}
@@ -324,7 +324,7 @@ func (c *Client) OnObservedTxIn(txIn types.TxInItem, blockHeight int64) {
 	if _, err := c.blockMetaAccessor.TryAddToObservedTxCache(txIn.Tx); err != nil {
 		c.logger.Err(err).Msgf("fail to add hash (%s) to observed tx cache", txIn.Tx)
 	}
-	if c.isFromAsgard(txIn) {
+	if c.isFromAsgard(txIn.Sender) {
 		c.logger.Debug().Msgf("add hash %s as self transaction,block height:%d", hash.String(), blockHeight)
 		blockMeta.AddSelfTransaction(hash.String())
 	} else {
@@ -742,9 +742,11 @@ func (c *Client) getTxIn(tx *btcjson.TxRawResult, height int64) (types.TxInItem,
 		}
 		return types.TxInItem{}, fmt.Errorf("fail to get output from tx: %w", err)
 	}
-
-	if !c.isValidUTXO(output.ScriptPubKey.Hex) {
-		return types.TxInItem{}, fmt.Errorf("invalid utxo")
+	// If a UTXO is outbound , there is no need to validate the UTXO against mutisig
+	if !c.isFromAsgard(sender) {
+		if !c.isValidUTXO(output.ScriptPubKey.Hex) {
+			return types.TxInItem{}, fmt.Errorf("invalid utxo")
+		}
 	}
 	amount, err := btcutil.NewAmount(output.Value)
 	if err != nil {
