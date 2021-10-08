b81b130063752b933d92052c8db84c138c4e7abe
Heimdall <heimdallthor02@gmail.com>
2021-10-09 06:40:04 +1100

    revert changes in https://gitlab.com/thorchain/thornode/-/commit/20b09fe7d4300d9a1bb9aba96f6cce4f4ece8e82 as this change made the encrypt / decrypt super slow

--- common/encryption.go
+++ common/encryption.go
@@ -3,36 +3,26 @@ package common
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/md5"
 	"crypto/rand"
-	"errors"
-	"golang.org/x/crypto/scrypt"
+	"encoding/hex"
 	"io"
 )
 
-const (
-	// parameters here are default parameters copied from go-ethereum
-	StandardScryptN = 1 << 18
-	scryptR         = 8
-	scryptDKLen     = 32
-	StandardScryptP = 1
-)
+func createHash(key string) (string, error) {
+	hasher := md5.New()
+	_, err := hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil)), err
+}
 
 // Encrypt the input data with passphrase
 func Encrypt(data []byte, passphrase string) ([]byte, error) {
-
-	if len(data) == 0 || len(passphrase) == 0 {
-		return nil, errors.New("data or passphrase should not be empty")
-	}
-	salt := make([]byte, 32)
-	if _, err := io.ReadFull(rand.Reader, salt); err != nil {
-		return nil, err
-	}
-	derivedKey, err := scrypt.Key([]byte(passphrase), salt, StandardScryptN, scryptR, StandardScryptP, scryptDKLen)
+	hash, err := createHash(passphrase)
 	if err != nil {
 		return nil, err
 	}
 
-	block, _ := aes.NewCipher(derivedKey)
+	block, _ := aes.NewCipher([]byte(hash))
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		return nil, err
@@ -44,21 +34,18 @@ func Encrypt(data []byte, passphrase string) ([]byte, error) {
 	}
 
 	ciphertext := gcm.Seal(nonce, nonce, data, nil)
-	ciphertext = append(salt, ciphertext...)
 	return ciphertext, nil
 }
 
 // Decrypt the input data with passphrase
 func Decrypt(data []byte, passphrase string) ([]byte, error) {
-	if len(data) < 32 || len(passphrase) == 0 {
-		return nil, errors.New("incorrect data format")
-	}
-	salt, encData := data[:32], data[32:]
-	derivedKey, err := scrypt.Key([]byte(passphrase), salt, StandardScryptN, scryptR, StandardScryptP, scryptDKLen)
+	hash, err := createHash(passphrase)
 	if err != nil {
 		return nil, err
 	}
-	block, err := aes.NewCipher(derivedKey)
+
+	key := []byte(hash)
+	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
 	}
@@ -68,7 +55,7 @@ func Decrypt(data []byte, passphrase string) ([]byte, error) {
 		return nil, err
 	}
 	nonceSize := gcm.NonceSize()
-	nonce, ciphertext := encData[:nonceSize], encData[nonceSize:]
+	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
 		return nil, err
