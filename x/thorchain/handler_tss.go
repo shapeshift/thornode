33bf676e4ecaec33eebb77644f1f6e4c17788718
Heimdall <heimdallthor02@gmail.com>
2021-11-02 12:15:32 +1100

    Update ConsensusChain to only require minority

--- x/thorchain/handler_tss.go
+++ x/thorchain/handler_tss.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/blang/semver"
+	"gitlab.com/thorchain/thornode/x/thorchain/types"
 
 	"gitlab.com/thorchain/thornode/common"
 	"gitlab.com/thorchain/thornode/common/cosmos"
@@ -185,7 +186,7 @@ func (h TssHandler) handleV72(ctx cosmos.Context, msg MsgTssPool) (*cosmos.Resul
 			if msg.KeygenType == AsgardKeygen {
 				vaultType = AsgardVault
 			}
-			chains := voter.ConsensusChains(HasSimpleMajority)
+			chains := voter.ConsensusChains(types.HasMinority)
 			vault := NewVault(common.BlockHeight(ctx), InitVault, vaultType, voter.PoolPubKey, chains.Strings(), h.mgr.Keeper().GetChainContracts(ctx, chains))
 			vault.Membership = voter.PubKeys
 
