489a4da905594434f781889139c0584ceeedf438
Pluto (9R) <pluto@ninerealms.co>
2021-12-28 09:46:57 -0500

    remove comment

--- x/thorchain/mimir_address_stagenet.go
+++ x/thorchain/mimir_address_stagenet.go
@@ -5,6 +5,6 @@ package thorchain
 
 // ADMINS hard coded admin address
 var ADMINS = []string{
-	"sthor1lr9hcvvkzclmcdke5kqx4wxeymeng36p7keq0f", // extra
-	"sthor1qwm26kmhctj5ezzrasz3cdccpt8kjjcj5fs0pn", // pluto
+	"sthor1lr9hcvvkzclmcdke5kqx4wxeymeng36p7keq0f",
+	"sthor1qwm26kmhctj5ezzrasz3cdccpt8kjjcj5fs0pn",
 }
