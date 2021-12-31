bf66f959f73b4c64a7b1a889e0621c767030d612
Son of Odin <sonofodin-thorchain@protonmail.com>
2021-12-31 15:58:10 -0500

    [bugfix] dedup erc20 list

--- scripts/lint-erc20s.bash
+++ scripts/lint-erc20s.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -s https://gitlab.com/thorchain/thornode/-/raw/develop/bifrost/pkg/chainclients/ethereum/token_list.json | jq -r '.tokens[] | .address | ascii_downcase' | sort -n >/tmp/orig_erc20_token_list.txt
+curl -s https://gitlab.com/thorchain/thornode/-/raw/develop/bifrost/pkg/chainclients/ethereum/token_list.json | jq -r '.tokens[] | .address | ascii_downcase' | sort -n | uniq -u >/tmp/orig_erc20_token_list.txt
 
-jq -r '.tokens[] | .address | ascii_downcase' <bifrost/pkg/chainclients/ethereum/token_list.json >/tmp/modified_erc20_token_list.txt
+jq -r '.tokens[] | .address | ascii_downcase' <bifrost/pkg/chainclients/ethereum/token_list.json | uniq -u >/tmp/modified_erc20_token_list.txt
 
 cat /tmp/orig_erc20_token_list.txt /tmp/modified_erc20_token_list.txt | sort -n | uniq -d >/tmp/union_erc20_token_list.txt
 
