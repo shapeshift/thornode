a87fb380fd776ca7ce08e15b88cd31c9638e7c52
Son of Odin <sonofodin-thorchain@protonmail.com>
2022-01-15 12:14:34 -0500

    Add THORName pre-registration addresses

--- x/thorchain/manager_store_chaosnet.go
+++ x/thorchain/manager_store_chaosnet.go
@@ -4,7 +4,20 @@
 package thorchain
 
 import (
+	"gitlab.com/thorchain/thornode/common"
 	"gitlab.com/thorchain/thornode/common/cosmos"
 )
 
 func migrateStoreV86(ctx cosmos.Context, mgr *Mgrs) {}
+
+func importPreRegistrationTHORNames(ctx cosmos.Context, mgr Manager) error {
+	names, err := getPreRegisterTHORNames(common.BlockHeight(ctx))
+	if err != nil {
+		return err
+	}
+
+	for _, name := range names {
+		mgr.Keeper().SetTHORName(ctx, name)
+	}
+	return nil
+}
