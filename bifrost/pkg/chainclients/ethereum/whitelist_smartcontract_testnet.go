016ad8e678fbdd1802db2e82e6a1ce1aa380ccd1
Heimdall <heimdallthor02@gmail.com>
2022-01-27 12:00:16 +1100

    Whitelist two aggregator contracts

--- bifrost/pkg/chainclients/ethereum/whitelist_smartcontract_testnet.go
+++ bifrost/pkg/chainclients/ethereum/whitelist_smartcontract_testnet.go
@@ -11,5 +11,9 @@ var (
 	whitelistSmartContractAddres = []common.Address{
 		// THORSwap Faucet
 		common.Address(`0x83b0c5136790dDf6cA8D3fb3d220C757e0a91fBe`),
+		// aggregator for uniswap v2
+		common.Address(`0x1E181dF53d07B698C6a58Ca6308AB5D827F116e1`),
+		// aggregator for uniswap v3
+		common.Address(`0x2F8aEdd149AFbDb5206ECaF8b1a3abB9186C8053`),
 	}
 )
