3b3a7338e113c5f40cf193c18d6fd0cd32134658
Heimdall <heimdallthor02@gmail.com>
2022-03-07 15:39:38 +1100

    Define coin purpose & coin type constant

--- cmd/cmd_stagenet.go
+++ cmd/cmd_stagenet.go
@@ -12,5 +12,6 @@ const (
 	Bech32PrefixConsPub         = "sthorcpub"
 	DenomRegex                  = `[a-zA-Z][a-zA-Z0-9:\\/\\\-\\_\\.]{2,127}`
 	THORChainCoinType    uint32 = 931
+	THORChainCoinPurpose uint32 = 44
 	THORChainHDPath      string = `m/44'/931'/0'/0/0`
 )
