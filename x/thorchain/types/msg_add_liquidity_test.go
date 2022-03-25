34359cc1a5bf967a032ed6d600c8f849349616ea
Heimdall <heimdallthor02@gmail.com>
2022-03-25 12:01:24 +1100

    Trying to fix tests

--- x/thorchain/types/msg_add_liquidity_test.go
+++ x/thorchain/types/msg_add_liquidity_test.go
@@ -90,9 +90,9 @@ func (MsgAddLiquiditySuite) TestMsgAddLiquidity(c *C) {
 			"",
 		)
 		m := NewMsgAddLiquidity(tx, item.asset, item.r, item.amt, item.runeAddr, item.assetAddr, common.NoAddress, cosmos.ZeroUint(), item.signer)
-		c.Assert(m.ValidateBasic(), NotNil, Commentf("%d) %s\n", i, m))
+		c.Assert(m.ValidateBasicV63(), NotNil, Commentf("%d) %s\n", i, m))
 	}
 	// If affiliate fee basis point is more than 1000 , the message should be rejected
 	m1 := NewMsgAddLiquidity(tx, common.BNBAsset, cosmos.NewUint(100*common.One), cosmos.NewUint(100*common.One), GetRandomTHORAddress(), GetRandomBNBAddress(), GetRandomTHORAddress(), cosmos.NewUint(1024), GetRandomBech32Addr())
-	c.Assert(m1.ValidateBasic(), NotNil)
+	c.Assert(m1.ValidateBasicV63(), NotNil)
 }
