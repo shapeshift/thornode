70c29de377dab2e635b50c2179b7134125c798f0
Pluto (9R) <pluto@ninerealms.co>
2022-03-27 20:29:04 -0400

    major version bump

--- x/thorchain/withdraw.go
+++ x/thorchain/withdraw.go
@@ -7,7 +7,7 @@ import (
 )
 
 func withdraw(ctx cosmos.Context, version semver.Version, msg MsgWithdrawLiquidity, manager Manager) (cosmos.Uint, cosmos.Uint, cosmos.Uint, cosmos.Uint, cosmos.Uint, error) {
-	if version.GTE(semver.MustParse("0.84.0")) {
+	if version.GTE(semver.MustParse("1.84.0")) {
 		return withdrawV84(ctx, version, msg, manager)
 	} else if version.GTE(semver.MustParse("0.76.0")) {
 		return withdrawV76(ctx, version, msg, manager)
