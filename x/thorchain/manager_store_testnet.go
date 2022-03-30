832aa8c6f58226440f21638b2bdce436fd006833
Heimdall <heimdallthor02@gmail.com>
2022-03-31 08:15:03 +1100

    Remote LTC from testnet

--- x/thorchain/manager_store_testnet.go
+++ x/thorchain/manager_store_testnet.go
@@ -18,3 +18,24 @@ func correctAsgardVaultBalanceV61(ctx cosmos.Context, mgr Manager, asgardPubKey 
 }
 
 func migrateStoreV80(ctx cosmos.Context, mgr Manager) {}
+
+// migrateStoreV86 remove all LTC asset from the retiring vault
+func migrateStoreV86(ctx cosmos.Context, mgr *Mgrs) {
+	defer func() {
+		if err := recover(); err != nil {
+			ctx.Logger().Error("fail to migrate store to v86", "error", err)
+		}
+	}()
+	vaults, err := mgr.Keeper().GetAsgardVaultsByStatus(ctx, RetiringVault)
+	if err != nil {
+		ctx.Logger().Error("fail to get retiring asgard vaults", "error", err)
+		return
+	}
+	for _, v := range vaults {
+		ltcCoin := v.GetCoin(common.LTCAsset)
+		v.SubFunds(common.NewCoins(ltcCoin))
+		if err := mgr.Keeper().SetVault(ctx, v); err != nil {
+			ctx.Logger().Error("fail to save vault", "error", err)
+		}
+	}
+}
