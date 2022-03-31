8fa7f01bf6de3c2412ca032c729f2a82e4b495bf
Son of Odin <sonofodin-thorchain@protonmail.com>
2022-03-31 19:21:02 -0400

    remove address change

--- common/address.go
+++ common/address.go
@@ -28,9 +28,6 @@ func NewAddress(address string) (Address, error) {
 	if len(address) == 0 {
 		return NoAddress, nil
 	}
-	if len(address) > 150 {
-		return NoAddress, nil
-	}
 
 	// Check is eth address
 	if eth.IsHexAddress(address) {
