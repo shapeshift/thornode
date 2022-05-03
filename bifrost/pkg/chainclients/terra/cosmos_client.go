3e2962c0c432f591b3f3a307c255a57cb7f5a04c
Ursa (9R) <ursa@ninerealms.co>
2022-05-03 17:16:29 +0000

    Increase Terra Broadcast Timeout

--- bifrost/pkg/chainclients/terra/cosmos_client.go
+++ bifrost/pkg/chainclients/terra/cosmos_client.go
@@ -498,7 +498,7 @@ func (c *CosmosClient) signMsg(
 
 // BroadcastTx is to broadcast the tx to cosmos chain
 func (c *CosmosClient) BroadcastTx(tx stypes.TxOutItem, txBytes []byte) (string, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
 
 	req := &txtypes.BroadcastTxRequest{
