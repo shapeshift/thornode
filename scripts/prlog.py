200d79ce6592a5d052d2530539bd91b6db8c26e4
Aquila (9R) <aquila@ninerealms.co>
2022-05-27 01:50:53 +0000

    [BUG] fix lint and search filter

--- scripts/prlog.py
+++ scripts/prlog.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
 # pip3 install gitlab-python
-import gitlab
 import sys
 
+import gitlab
+
 if len(sys.argv) == 1:
-    exit('usage: ./prlog <milestone name>')
+    exit("usage: ./prlog <milestone name>")
 
 gl = gitlab.Gitlab()
-tn = gl.projects.get(13422983) # thornode project id
+tn = gl.projects.get(13422983)  # thornode project id
 
-mrs = tn.mergerequests.list(milestone=sys.argv[1], status='Merged', all=True)
+mrs = tn.mergerequests.list(milestone=sys.argv[1], state="closed", all=True)
 
 for idx, m in enumerate(mrs):
     print(f"{idx+1}) {m.title} PR: {m.web_url}")
-
