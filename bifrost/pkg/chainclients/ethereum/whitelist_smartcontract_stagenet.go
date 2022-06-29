578dd84850f75a1b425bb5b9f6bd158d2b42ae01
Ursa (9R) <ursa@ninerealms.co>
2022-06-29 20:19:59 +0000

    Stagenet and Rango Aggregator Contracts

--- bifrost/pkg/chainclients/ethereum/whitelist_smartcontract_stagenet.go
+++ bifrost/pkg/chainclients/ethereum/whitelist_smartcontract_stagenet.go
@@ -7,13 +7,14 @@ import (
 	"gitlab.com/thorchain/thornode/common"
 )
 
-var whitelistSmartContractAddress = []common.Address{
-	// XRUNE
-	common.Address(`0x69fa0feE221AD11012BAb0FdB45d444D3D2Ce71c`),
-	// THORSwap Faucet
-	common.Address(`0xB73B8E66196f2AF0762833304e3f15dB2e8Df0c3`),
-	// THORSwap Wrapped Uniswap V2 Router
-	common.Address(`0x1e181df53d07b698c6a58ca6308ab5d827f116e1`),
-	// THORSwap Wrapped Uniswap V3 Router
-	common.Address(`0x2f8aedd149afbdb5206ecaf8b1a3abb9186c8053`),
-}
+var whitelistSmartContractAddress = append(
+	[]common.Address{
+		// XRUNE
+		common.Address(`0x69fa0feE221AD11012BAb0FdB45d444D3D2Ce71c`),
+		// THORSwap Faucet
+		common.Address(`0xB73B8E66196f2AF0762833304e3f15dB2e8Df0c3`),
+		// RangoV1
+		common.Address(`0x0e3EB2eAB0e524b69C79E24910f4318dB46bAa9c`),
+	},
+	LatestAggregatorContracts()...,
+)
