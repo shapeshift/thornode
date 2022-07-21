5030e65bfbe3554e7aea93d728f5bd18224a3ba0
Aquila (9R) <aquila@ninerealms.co>
2022-07-20 21:59:56 -0600

    Fix linter on v93

--- x/thorchain/manager_network_v93.go
+++ x/thorchain/manager_network_v93.go
@@ -727,7 +727,7 @@ func (vm *NetworkMgrV93) UpdateNetwork(ctx cosmos.Context, constAccessor constan
 			if !pool.IsAvailable() {
 				continue
 			}
-			amt := cosmos.ZeroUint()
+			var amt cosmos.Uint
 			if totalLiquidityFees.IsZero() {
 				amt = common.GetSafeShare(pool.BalanceRune, totalProvidedLiquidity, totalPoolRewards)
 			} else {
