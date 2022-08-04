609ccdfa741d58b95b8d7e7ee4d7656fc71753e0
Son of Odin <2700975-son-of-odin@users.noreply.gitlab.com>
2022-08-04 19:28:06 +0000

    [refactor] Effective bond cap

--- x/thorchain/managers.go
+++ x/thorchain/managers.go
@@ -329,6 +329,8 @@ func GetNetworkManager(version semver.Version, keeper keeper.Keeper, txOutStore 
 // GetValidatorManager create a new instance of Validator Manager
 func GetValidatorManager(version semver.Version, keeper keeper.Keeper, networkMgr NetworkManager, txOutStore TxOutStore, eventMgr EventManager) (ValidatorManager, error) {
 	switch {
+	case version.GTE(semver.MustParse("1.95.0")):
+		return newValidatorMgrV95(keeper, networkMgr, txOutStore, eventMgr), nil
 	case version.GTE(semver.MustParse("1.92.0")):
 		return newValidatorMgrV92(keeper, networkMgr, txOutStore, eventMgr), nil
 	case version.GTE(semver.MustParse("1.87.0")):
