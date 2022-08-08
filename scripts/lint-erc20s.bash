2fa9a2ca429543a4e34f0144f45019e584ee5118
Asmund <9491667-AsmundTHORSec@users.noreply.gitlab.com>
2022-08-08 21:11:32 +0000

    Fix lint, enable AVAX token list checking

--- scripts/lint-erc20s.bash
+++ scripts/lint-erc20s.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-git show origin/develop:common/tokenlist/eth_mainnet_V93.json |
+# Ethereum tokens
+git show origin/develop:common/tokenlist/ethtokens/eth_mainnet_V93.json |
   jq -r '.tokens[] | .address | ascii_downcase' | sort -n | uniq -u >/tmp/orig_erc20_token_list.txt
 
 jq -r '.tokens[] | .address | ascii_downcase' <common/tokenlist/ethtokens/eth_mainnet_V93.json |
@@ -12,17 +13,16 @@ cat /tmp/orig_erc20_token_list.txt /tmp/modified_erc20_token_list.txt |
 
 diff /tmp/orig_erc20_token_list.txt /tmp/union_erc20_token_list.txt || exit 1
 
-# TODO: uncomment when this PR is merged
 # AVAX Tokens
-# git show origin/develop:common/tokenlist/avaxtokens/avax_mainnet_V95.json |
-#   jq -r '.tokens[] | .address | ascii_downcase' | sort -n | uniq -u >/tmp/orig_avax_erc20_token_list.txt
+git show origin/develop:common/tokenlist/avaxtokens/avax_mainnet_V95.json |
+  jq -r '.tokens[] | .address | ascii_downcase' | sort -n | uniq -u >/tmp/orig_avax_erc20_token_list.txt
 
-# jq -r '.tokens[] | .address | ascii_downcase' <common/tokenlist/avaxtokens/avax_mainnet_V95.json |
-#   uniq -u >/tmp/modified_avax_erc20_token_list.txt
+jq -r '.tokens[] | .address | ascii_downcase' <common/tokenlist/avaxtokens/avax_mainnet_V95.json |
+  uniq -u >/tmp/modified_avax_erc20_token_list.txt
 
-# cat /tmp/orig_avax_erc20_token_list.txt /tmp/modified_avax_erc20_token_list.txt |
-#   sort -n | uniq -d >/tmp/union_avax_erc20_token_list.txt
+cat /tmp/orig_avax_erc20_token_list.txt /tmp/modified_avax_erc20_token_list.txt |
+  sort -n | uniq -d >/tmp/union_avax_erc20_token_list.txt
 
-# diff /tmp/orig_avax_erc20_token_list.txt /tmp/union_avax_erc20_token_list.txt || exit 1
+diff /tmp/orig_avax_erc20_token_list.txt /tmp/union_avax_erc20_token_list.txt || exit 1
 
 echo "OK"
