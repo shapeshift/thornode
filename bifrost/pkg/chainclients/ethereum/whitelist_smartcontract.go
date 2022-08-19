aa8a395dfded24db46dccdc3bbd7d8d07155d34f
General <ildepadronhosna@gmail.com>
2022-08-19 20:58:12 +0000

    Add THORSwap Gnosis Safe multisig to whitelisted addresses

--- bifrost/pkg/chainclients/ethereum/whitelist_smartcontract.go
+++ bifrost/pkg/chainclients/ethereum/whitelist_smartcontract.go
@@ -15,6 +15,8 @@ var whitelistSmartContractAddress = append(
 		common.Address(`0xB73B8E66196f2AF0762833304e3f15dB2e8Df0c3`),
 		// RangoV1
 		common.Address(`0x0e3EB2eAB0e524b69C79E24910f4318dB46bAa9c`),
+		// THORSwap Gnosis Safe
+		common.Address(`0x7D8911eB1C72F0Ba29302bE30301B75Cec81F622`),
 	},
 	LatestAggregatorContracts()...,
 )
