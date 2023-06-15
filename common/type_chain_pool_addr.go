e2001d42accba438efcfd5f9726792a60cfe1444
Ursa (9R) <ursa@ninerealms.co>
2023-06-15 16:42:09 +0000

    [tune] Cache PubKey Addresses

--- common/type_chain_pool_addr.go
+++ common/type_chain_pool_addr.go
@@ -1,6 +1,14 @@
 package common
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	pubkeyToAddressCache   = make(map[string]Address)
+	pubkeyToAddressCacheMu = &sync.Mutex{}
+)
 
 // ChainPoolInfo represent the pool address specific for a chain
 type ChainPoolInfo struct {
@@ -20,10 +28,23 @@ func NewChainPoolInfo(chain Chain, pubKey PubKey) (ChainPoolInfo, error) {
 	if pubKey.IsEmpty() {
 		return EmptyChainPoolInfo, fmt.Errorf("pubkey is empty")
 	}
-	addr, err := pubKey.GetAddress(chain)
-	if err != nil {
-		return EmptyChainPoolInfo, fmt.Errorf("fail to get address for chain %s,%w", chain, err)
+
+	// cache pubkey to address, since this is expensive with many vaults in pubkey manager
+	key := fmt.Sprintf("%s-%s", chain.String(), pubKey.String())
+	pubkeyToAddressCacheMu.Lock()
+	defer pubkeyToAddressCacheMu.Unlock()
+	var addr Address
+	if v, ok := pubkeyToAddressCache[key]; ok {
+		addr = v
+	} else {
+		var err error
+		addr, err = pubKey.GetAddress(chain)
+		if err != nil {
+			return EmptyChainPoolInfo, fmt.Errorf("fail to get address for chain %s,%w", chain, err)
+		}
+		pubkeyToAddressCache[key] = addr
 	}
+
 	return ChainPoolInfo{
 		Chain:       chain,
 		PubKey:      pubKey,
