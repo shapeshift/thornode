ae39e3e4e2727b75741a5c4ecfc7183c186f6595
Multipartite <sonaebito@gmail.com>
2023-08-03 05:41:41 +0000

    [V117-specific] Equal migration rounds

--- x/thorchain/managers.go
+++ x/thorchain/managers.go
@@ -407,8 +407,10 @@ func GetTxOutStore(version semver.Version, keeper keeper.Keeper, eventMgr EventM
 // GetNetworkManager  retrieve a NetworkManager that is compatible with the given version
 func GetNetworkManager(version semver.Version, keeper keeper.Keeper, txOutStore TxOutStore, eventMgr EventManager) (NetworkManager, error) {
 	switch {
-	case version.GTE(semver.MustParse("1.115.0")):
+	case version.GTE(semver.MustParse("1.117.0")):
 		return newNetworkMgrVCUR(keeper, txOutStore, eventMgr), nil
+	case version.GTE(semver.MustParse("1.115.0")):
+		return newNetworkMgrV115(keeper, txOutStore, eventMgr), nil
 	case version.GTE(semver.MustParse("1.112.0")):
 		return newNetworkMgrV112(keeper, txOutStore, eventMgr), nil
 	case version.GTE(semver.MustParse("1.111.0")):
